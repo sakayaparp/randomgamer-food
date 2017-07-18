77dd999544c6b87ee01be139fbf4a0a7fb3f67cb
gamer <sakayaparp.pon@ascendcorp.com>
2017-07-18 15:18:05 +0700

    Change layout page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
-    </main>
-  </div>
+    <div id="app">
+      <header>
+        <span>Randomgamer Food</span>
+      </header>
+      <main>
+        <router-view></router-view>
+      </main>
+      <footer>
+        <span>All right reserved &copy by randomgamer</span>
+      </footer>
+    </div>
 </template>
 
 <script>
@@ -26,6 +28,13 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  width: 80%;
+  margin-left: 10%;
+  background-color: aliceblue;
+  -webkit-box-shadow: 0px 0px 112px 8px rgba(0,0,0,1);
+  -moz-box-shadow: 0px 0px 112px 8px rgba(0,0,0,1);
+  box-shadow: 0px 0px 112px 8px rgba(0,0,0,1);
+  height: 100%;
 }
 
 main {
@@ -33,7 +42,7 @@ main {
   margin-top: 40px;
 }
 
-header {
+header, footer {
   margin: 0;
   height: 56px;
   padding: 0 16px 0 24px;
@@ -41,14 +50,30 @@ header {
   color: #ffffff;
 }
 
+footer {
+  margin-top: 40px;
+  text-align: center;
+}
+
 header span {
   display: block;
   position: relative;
   font-size: 20px;
   line-height: 1;
-  letter-spacing: .02em;
+  letter-spacing: 1em;
   font-weight: 400;
   box-sizing: border-box;
   padding-top: 16px;
+}
+
+footer span {
+  display: block;
+  position: relative;
+  line-height: 1;
+  letter-spacing: 1em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+  align-content: center;
 }
 </style>
